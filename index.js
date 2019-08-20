e4e99070b16b4f0fae98ad6a3c85dd459cab65fc
rhysgoehring <rhysgoehring@gmail.com>
2019-08-20 12:59:47 -0600

    created basic user model

--- index.js
+++ index.js
@@ -7,15 +7,16 @@ require('dotenv').config();
 const app = express();
 app.use(express.json());
 
-
-
-mongoose.connect(
-  `mongodb+srv://rhysgoehring:${
-    process.env.MONGO_PASSWORD
-  }@streamchat-zwel6.mongodb.net/test?retryWrites=true&w=majority
+mongoose
+  .connect(
+    `mongodb+srv://rhysgoehring:${
+      process.env.MONGO_PASSWORD
+    }@streamchat-zwel6.mongodb.net/test?retryWrites=true&w=majority
 `,
-  { useNewUrlParser: true },
-);
+    { useNewUrlParser: true },
+  )
+  .then(() => console.log('Connected to MongoDB'))
+  .catch((err) => console.log('MongoDB Connection Error: ', err));
 
 app.listen(5000, () => {
   console.log('Server is running on port 5000');
---
+++ models/user.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
+
+const UserSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    minlength: 3,
+    maxlength: 15,
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 3,
+    maxlength: 255,
+  },
+  dateCreated: {
+    type: Date,
+    default: Date.now(),
+  },
+});
+
+UserSchema.methods.generateAuthToken = () => {
+  const token = jwt.sign(
+    { _id: this._id, username: this.username, dateCreated: this.dateCreated },
+    process.env.JWT_KEY,
+  );
+  return token;
+};
