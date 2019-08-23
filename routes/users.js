e2c11041fe999bbdc6d104bdc89fb37655a50cac
rhysgoehring <rhysgoehring@gmail.com>
2019-08-23 09:57:43 -0600

    modified signin and signup routes to generate chatToken and send in response

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 const bcrypt = require("bcryptjs");
 const express = require("express");
+const { StreamChat } = require("stream-chat");
 const { User } = require("../models/user");
 
 const router = express.Router();
@@ -7,7 +8,7 @@ const router = express.Router();
 router.get("/", (req, res) => res.send("It's working"));
 
 router.post("/signup", async (req, res) => {
-  const { username, password, dateCreated } = req.body;
+  const { username, password } = req.body;
   try {
     // Check if username is taken:
     let user = await User.findOne({ username });
@@ -17,16 +18,21 @@ router.post("/signup", async (req, res) => {
     user = new User({
       username,
       password,
-      dateCreated,
     });
 
     // Hash Password & Save User
     user.password = await bcrypt.hash(user.password, 10);
     await user.save();
 
+    // Create StreamChat token:
+    const serverSideClient = new StreamChat(process.env.STREAM_KEY, process.env.STREAM_SECRET);
+    const chatToken = serverSideClient.createToken(user.id);
+
     // Generate token and send response
     const token = user.generateAuthToken();
-    return res.status(200).send({ _id: user.id, username: user.username, token });
+    return res.status(200).send({
+      _id: user.id, username: user.username, token, chatToken,
+    });
   } catch (err) {
     console.log("user signup error", err);
   }
@@ -45,9 +51,18 @@ router.post("/signin", async (req, res) => {
       if (!isMatch) {
         return res.status(400).json({ error: "Invalid Password." });
       }
-      // Generate token and and send response
+      // Create StreamChat token:
+      const serverSideClient = new StreamChat(process.env.STREAM_KEY, process.env.STREAM_SECRET);
+      const chatToken = serverSideClient.createToken(user.id);
+
+      // Generate auth token and and send response
       const token = user.generateAuthToken();
-      return res.status(200).send({ _id: user.id, username: user.username, token });
+      return res.status(200).send({
+        _id: user.id,
+        username: user.username,
+        token,
+        chatToken,
+      });
     });
   } catch (err) {
     console.log("user sign in error: ", err);
